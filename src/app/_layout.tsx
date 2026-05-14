41babf2b65fa1325195b44d321aabc1ce8dc21d2
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-05-14 17:01:17 -0300

    feat: add the fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,30 @@
+import * as SplashScreen from "expo-splash-screen";
 import { Text, View } from "react-native";
 import "../../global.css";
- 
+
+import { Lemon_400Regular } from "@expo-google-fonts/lemon";
+import { Poppins_400Regular, Poppins_700Bold } from "@expo-google-fonts/poppins";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Lemon_400Regular,
+    Poppins_400Regular,
+    Poppins_700Bold
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError)
+      SplashScreen.hideAsync();
+  }, [fontsLoaded, fontError]);
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Good Diary!
+      <Text className="text-xl font-lemon text-blue-500">
+        Good Diary
       </Text>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lemon: ["Lemon_400Regular"],
+        poppins: ["Poppins_400Regular"],
+        "poppins-bold": ["Poppins_700Bold"]
+      }
+    },
   },
   plugins: [],
 }
